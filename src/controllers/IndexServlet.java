8efbd7624b8e95ee53b49d8c23fab4b15f91a207
mentor-higud <dice.if@gmail.com>
2021-03-25 10:15:09 +0900

    Add pagination

--- src/controllers/IndexServlet.java
+++ src/controllers/IndexServlet.java
@@ -21,28 +21,50 @@ import utils.DBUtil;
 public class IndexServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 
-    /**
-     * @see HttpServlet#HttpServlet()
-     */
-    public IndexServlet() {
-        super();
-        // TODO Auto-generated constructor stub
-    }
+	/**
+	 * @see HttpServlet#HttpServlet()
+	 */
+	public IndexServlet() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
 
 	/**
-	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse
+	 *      response)
 	 */
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-	    EntityManager em = DBUtil.createEntityManager();
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		EntityManager em = DBUtil.createEntityManager();
 
-	    List<Message> messages = em.createNamedQuery("getAllMessages", Message.class).getResultList();
+		// 開くページ数を取得（デフォルトは1ページ目）
+		int page = 1;
+		try {
+			page = Integer.parseInt(request.getParameter("page"));
+		} catch (NumberFormatException e) {
+		}
 
-	    em.close();
+		// 最大件数と開始位置を指定してメッセージを取得
+		List<Message> messages = em.createNamedQuery("getAllMessages", Message.class).setFirstResult(15 * (page - 1))
+				.setMaxResults(15).getResultList();
 
-	    request.setAttribute("messages", messages);
+		// 全件数を取得
+		long messages_count = (long) em.createNamedQuery("getMessagesCount", Long.class).getSingleResult();
 
-	    RequestDispatcher rd = request.getRequestDispatcher("/WEB-INF/views/messages/index.jsp");
-	    rd.forward(request, response);
+		em.close();
+
+		request.setAttribute("messages", messages);
+		request.setAttribute("messages_count", messages_count); // 全件数
+		request.setAttribute("page", page); // ページ数
+
+		// フラッシュメッセージがセッションスコープにセットされていたら
+		if (request.getSession().getAttribute("flush") != null) {
+			// セッションスコープ内のフラッシュメッセージをリクエストスコープに保存し、セッションスコープからは削除する
+			request.setAttribute("flush", request.getSession().getAttribute("flush"));
+			request.getSession().removeAttribute("flush");
+		}
+		RequestDispatcher rd = request.getRequestDispatcher("/WEB-INF/views/messages/index.jsp");
+		rd.forward(request, response);
 	}
 
 }
